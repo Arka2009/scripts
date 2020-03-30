06c4509f54f44f7e8be5fa94b108eaf9fc41015f
Arka Maity <arka.maity09@gmail.com>
2020-03-31 02:41:13 +0800

    more bug fixes

--- cloneLinuxMainline.sh
+++ cloneLinuxMainline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Clone the kernel to your local machine
-git clone https://github.com/torvalds/linux.git linux-5.6
-cd linux-5.6
+git clone git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+cd linux-stable
 
 # Find the tag for the version you want
 $ git tag -l | grep 5.6
